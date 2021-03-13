8b1aa47fbc3b1d95bae7267c2a2a749fe6ecb48b
Abdullah Rehman <ayr18@ic.ac.uk>
2021-03-13 18:33:47 +0000

    NFC functionality update

--- android/src/main/java/crescentech/NFCPower.java
+++ android/src/main/java/crescentech/NFCPower.java
@@ -5,6 +5,9 @@ import android.Manifest;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+
+
+import android.nfc.FormatException;
 import android.util.Log;
 
 import com.getcapacitor.JSObject;
@@ -15,6 +18,8 @@ import com.getcapacitor.PluginMethod;
 
 import android.nfc.Tag;
 import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.Ndef;
+import android.nfc.NdefMessage;
 import android.util.Log;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -29,5 +34,38 @@ public class NFCPower extends Plugin {
         JSObject ret = new JSObject();
         ret.put("value", value);
         call.success(ret);
+
+        new Thread(new Runnable() {
+            private Tag tag;
+
+            public void run() {
+                Ndef ndef = Ndef.get(tag);
+
+                try {
+                    while (true) {
+                        try {
+                            Thread.sleep(30000);
+
+                            ndef.connect();
+                            NdefMessage msg = ndef.getNdefMessage();
+
+                            // TODO: do something
+
+                        } catch (IOException e) {
+                            // if the tag is gone we might want to end the thread:
+                            break;
+                        } finally {
+                            try {
+                                ndef.close();
+                            } catch (Exception e) {}
+                        }
+                    }
+                } catch (InterruptedException | FormatException e) {
+                }
+            }
+        }).start();
     }
+
+    //https://stackoverflow.com/questions/37045486/continuously-detect-an-nfc-tag-on-android
+
 }
